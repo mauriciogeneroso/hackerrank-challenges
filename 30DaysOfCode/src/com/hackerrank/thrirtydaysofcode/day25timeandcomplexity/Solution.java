0a14bd7bc65a542f111279ca23ddea2ec48dee65
Mauricio Generoso <mauro_generoso@hotmail.com>
2021-01-09 13:42:38 +0000

    30 days of code: 25 running time and complexity

---
+++ 30DaysOfCode/src/com/hackerrank/thrirtydaysofcode/day25timeandcomplexity/Solution.java
@@ -0,0 +1,34 @@
+package com.hackerrank.thrirtydaysofcode.day25timeandcomplexity;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        int[] numbers = new int[T];
+
+        for (int i = 0; i < T; i++) {
+            numbers[i] = scan.nextInt();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            boolean prime = true;
+            for (int j = 2; j <= numbers[i] / 2; j++) {
+                if (numbers[i] % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if (numbers[i] != 1 && prime) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not prime");
+            }
+        }
+    }
+}
+
