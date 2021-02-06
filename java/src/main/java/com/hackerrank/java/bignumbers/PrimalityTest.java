e683c02f3133811a003face85cd124eb49e48ad5
Mauricio Generoso <mauro_generoso@hotmail.com>
2021-02-06 16:08:58 +0000

    solved java bigintegers and primality test

---
+++ java/src/main/java/com/hackerrank/java/bignumbers/PrimalityTest.java
@@ -0,0 +1,16 @@
+package com.hackerrank.java.bignumbers;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class PrimalityTest {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+        BigInteger b = new BigInteger(n);
+        System.out.println(b.isProbablePrime(1) ? "prime" : "not prime");
+        scanner.close();
+    }
+}
