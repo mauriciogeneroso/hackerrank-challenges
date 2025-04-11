acf4fb898a1a331029db6c7a1120e790ee2bccf7
Mauricio Generoso <mauricio.generoso@hotmail.com>
2025-04-11 23:43:38 +0100

    Review some java introduction exercises

--- solutions/src/main/java/com/generoso/hackerrank/java/introduction/LoopsII.java
+++ solutions/src/main/java/com/generoso/hackerrank/java/introduction/LoopsII.java
@@ -12,15 +12,36 @@ public class LoopsII {
             int b = in.nextInt();
             int n = in.nextInt();
 
+            int sum = a;
+            int powerOfTwo = 1;
+
             for (int j = 0; j < n; j++) {
-                int nextB = 0;
-                for (int k = 0; k <= j; k++) {
-                    nextB += Math.pow(2, k) * b;
-                }
-                System.out.printf("%d ", (a + nextB));
+                sum += powerOfTwo * b;
+                System.out.printf("%d ", sum);
+                powerOfTwo *= 2;
             }
             System.out.println();
         }
         in.close();
     }
+
+//    public static void main(String[] argh) {
+//        Scanner in = new Scanner(System.in);
+//        int t = in.nextInt();
+//        for (int i = 0; i < t; i++) {
+//            int a = in.nextInt();
+//            int b = in.nextInt();
+//            int n = in.nextInt();
+//
+//            for (int j = 0; j < n; j++) {
+//                int sum = a;
+//                for (int k = 0; k <= j; k++) {
+//                    sum += (int) (Math.pow(2, k) * b);
+//                }
+//                System.out.printf("%d ", sum);
+//            }
+//            System.out.println();
+//        }
+//        in.close();
+//    }
 }
