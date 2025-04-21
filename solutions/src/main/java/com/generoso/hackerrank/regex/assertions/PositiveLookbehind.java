542943a10b4f12abbf5116461fc428527bba0ab6
Mauricio Generoso <mauricio.generoso@hotmail.com>
2025-04-21 09:56:31 +0100

    [Regex] Review assertions

--- solutions/src/main/java/com/generoso/hackerrank/regex/assertions/PositiveLookbehind.java
+++ solutions/src/main/java/com/generoso/hackerrank/regex/assertions/PositiveLookbehind.java
@@ -6,6 +6,6 @@ public class PositiveLookbehind {
 
     public static void main(String[] args) {
         RegexTest tester = new RegexTest();
-        tester.checker("(?<=[1,3,5,7,9])[0-9]");
+        tester.checker("(?<=[13579])\\d");
     }
 }
